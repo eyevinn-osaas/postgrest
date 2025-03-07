940c9761f510387916886f75805da053be11f6f0
Jonas Birmé <jonas.birme@eyevinn.se>
2025-03-07 17:27:22 +0100

    feat: can provide schemas and anon role with env variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,12 @@ cd $HOME
 LISTEN_PORT=${PORT:-8080}
 postgrest -e > ./postgrest.conf
 sed -i "s/server-port = 3000/server-port = ${LISTEN_PORT}/" ./postgrest.conf
+if [[ ! -z "$DB_ANON_ROLE" ]]; then 
+  sed -i "s/# db-anon-role = \"anon\"/db-anon-role = \"${DB_ANON_ROLE}\"/" ./postgrest.conf
+fi
+if [[ ! -z "$DB_SCHEMAS" ]]; then 
+  sed -i "s/db-schemas = \"public\"/db-schemas = \"${DB_SCHEMAS}\"/" ./postgrest.conf
+fi
 
 if [[ ! -z "$@" ]]; then
   exec "$@"
